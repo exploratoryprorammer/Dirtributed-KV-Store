f7055ebb84024aa05338a1f5fc6efad8858009cb
exploratoryprorammer <rohan.k.sah@gmail.com>
2026-01-25 14:10:20 -0600

    election logic implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+mod node;
+
 fn main() {
-    println!("Hello world ")
+    println!("Welcome to the distributed systems raft simulator")
+}
+
+loop {
+    node.tick();
+    std::thread::sleep(Duration::from_millis(10))
 }
--- src/node.rs
+++ src/node.rs
@@ -1,3 +1,4 @@
+
 pub enum Role {
     Leader,
     Follower,
@@ -13,6 +14,33 @@ pub struct RaftNode {
     pub votes_recieved: u64,
 }
 
+impl RaftNode {
+    pub fn tick(&mut self) {
+        if Instant::now() >= slef.election_deadline {
+            self.start_election();
+        }
+    }
+}
+
+impl RaftNode {
+    fn start_election(&mut self) {
+        self.role = Role::Candidate;
+        self.current_term += 1;
+        self.voted_for = Some(self.id);
+        self.votes_recieved = 1;
+
+        self.election_deadline = Instant::now() + random_election_timeout();
+
+        println!(
+            "[Node {}] Starting election for term {}",
+            self.id, self.current_term
+        );
+    }
+}
+
+
+
+
 use rand::Rng;
 use std::time::Duration;
 use std::time::Instant;
@@ -21,3 +49,10 @@ fn random_election_timeout() -> Duration {
     let mut rng = rand::thread_rng();
     Duration::from_millis(rng.gen_rage(150..300))
 }
+
+self.election_deadline = Instant::now() + random_election_timeout();
+
+
+
+
+
