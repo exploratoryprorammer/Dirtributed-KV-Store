518845f5641d0cc93c04dd2773e57ef6f4a29c42
exploratoryprorammer <rohan.k.sah@gmail.com>
2026-01-31 16:18:33 -0600

    more documentation on this

--- src/rpc.rs
+++ src/rpc.rs
@@ -1,4 +1,8 @@
 #[derive(Debug, Clone)]
+//In distributed systems we must used RPCs as we do not know the state of the of a different dsystem and we may disrupt the process of that sustem.
+//So instead we send a message describing the process to that other system.
+//Each enum variant meants if this were a local system. I would call this method
+
 
 pub enum Rpc {
     RequestVote {
